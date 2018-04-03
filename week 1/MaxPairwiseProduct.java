a24c21922222e2007a988a62ea5740847eba6e7d
hyde1985 <hyde_sun@sina.com>
2018-04-03 10:32:22 +0800

    Finish MaxPairwiseProduct with even Faster Algorithm

--- week 1/MaxPairwiseProduct.java
+++ week 1/MaxPairwiseProduct.java
@@ -3,27 +3,8 @@ import java.io.*;
 
 public class MaxPairwiseProduct {
      static long getMaxPairwiseProduct(int[] numbers) {
-         if(number.length == 2) {
-             return Long.valueOf(numbers[0]) * numbers[1];
-         }
-         Long result = Long.valueOf(0);
-         int index = 0;
-         int maxNum = numbers[index];
-         int secondMax = numbers[0];
-         for(int i = 1; i < numbers.length; i++) {
-             if(numbers[i] > maxNum) {
-                 maxNum = numbers[i];
-                 index = i;
-             }
-         }
-         for(int i = 1; i < numbers.length; i++) {
-             if(i != index) {
-                 if(numbers[i] > secondMax && numbers[i] <= maxNum) {
-                     secondMax = numbers[i];
-                 }
-             }
-         }
-         return Long.valueOf(maxNum) * secondMax;
+         Arrays.sort(numbers);
+         return Long.valueOf(numbers[numbers.length - 1]) * numbers[numbers.length - 2];
      }
 
     public static void main(String[] args) {
